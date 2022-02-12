aba9c2677e231fefe89984875cc38231185a33ad
Kevin Santiago <44878612+KevinSantiago51@users.noreply.github.com>
2022-02-12 15:55:31 -0400

    Add files via upload

---
+++ WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Kevin Santiago on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
